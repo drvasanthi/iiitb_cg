dc99c42a0bc47d90660dfbde988e193317cb2c96
drvasanthi <67214592+drvasanthi@users.noreply.github.com>
2022-07-27 10:01:04 +0530

    Create iiitb+cg_tb.v

---
+++ iiitb+cg_tb.v
@@ -0,0 +1,17 @@
+module clock_delay_tb();
+	reg d,en,clk;
+	wire q,gclk;
+	clock_delay test(d,en,clk,q,gclk);
+	initial
+	begin
+	d=0;
+	en=0;
+	clk=0;
+	$dumpfile("test.vcd");
+	$dumpvars(1);
+	#200 $finish;
+	end
+	always #5 d<=~d;
+	always #12 en<=~en;
+	always #3 clk=~clk;
+	endmodule
