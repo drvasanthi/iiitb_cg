375cba5cc8dca18b8b97ece14504832f96b20478
drvasanthi <67214592+drvasanthi@users.noreply.github.com>
2022-08-17 21:53:27 +0530

    Update iiitb_icg_tb.v

--- verilog/iiitb_icg_tb.v
+++ verilog/iiitb_icg_tb.v
@@ -4,19 +4,20 @@ module iiitb_icg_tb();
     reg in, clk, d0, d1;
     wire q0, q1;
     
-    iiitb_icg test (.in(in), .clk(clk), .d0(d0), .d1(d1), .q0(q0), .q1(q1));
+    iiitb_icg uut (.in(in), .clk(clk), .d0(d0), .d1(d1), .q0(q0), .q1(q1));
     initial 
     begin
-    
+    $dumpfile("iiitb_icg_tb.vcd");
+	$dumpvars(0,iiitb_icg_tb);
+	// Initialize Inputs
     in = 0;
     d0 = 0;
     d1 = 0;
     clk = 0;
     //en=0;
     
-    $dumpfile("test.vcd");
-	$dumpvars(1);
-	#200 $finish;
+    
+	#3000 $finish;
 	end
 	
 	always #20 in<=~in;
