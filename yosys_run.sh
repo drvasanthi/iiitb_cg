951a20ab641c6524838b5dfcb656ce92e55bec8e
drvasanthi <67214592+drvasanthi@users.noreply.github.com>
2022-08-22 10:50:53 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,14 +1,19 @@
 # read design
   
-read_verilog /home/vasanthidr11/Desktop/iiitb_cg/verilog/iiitb_cg.v
+read_verilog /home/vasanthi/Desktop/iiitb_cg/verilog/iiitb_icg.v
+read_verilog /home/vasanthi/Desktop/iiitb_cg/verilog/dff.v
+
 
 # generic synthesis
-synth -top iiitb_cg
+synth -top iiitb_icg
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/vasanthidr11/Desktop/iiitb_cg/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/vasanthidr11/Desktop/iiitb_cg/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+dfflibmap -liberty /home/vasanthi/Desktop/iiitb_cg/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/vasanthi/Desktop/iiitb_cg/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+# opt
 clean
 flatten
 # write synthesized design
-write_verilog iiitb_cg_synth.v
+write_verilog -noattr iiitb_icg_synth.v
+
+stat
